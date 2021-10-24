fc3ed28d6cb4a8e2d9067a5c9c1aba8d930c0ae6
GopiNath321 <87842167+GopiNath321@users.noreply.github.com>
2021-10-24 23:13:05 +0530

    Update Message.java

--- src/main/java/com/example/demo/Message.java
+++ src/main/java/com/example/demo/Message.java
@@ -2,27 +2,13 @@ package com.example.demo;
 
 public class Message {
 
-	String connectionString;
-	String queueName; 
-	String message;
+	String data;
 	
-	public String getConnectionString() {
-		return connectionString;
+	public String getData() {
+		return data;
 	}
-	public void setConnectionString(String connectionString) {
-		this.connectionString = connectionString;
-	}
-	public String getQueueName() {
-		return queueName;
-	}
-	public void setQueueName(String queueName) {
-		this.queueName = queueName;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
+	public void setData(String data) {
+		this.data = data;
 	}
 	
 	
